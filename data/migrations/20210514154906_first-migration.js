74400ab6021e56672a6185c5b2a9c47c3b7b8fa6
Francis Nguyen <francishtknguyen@gmail.com>
2021-05-14 12:20:57 -0500

    wsc - db updated seed data

--- data/migrations/20210514154906_first-migration.js
+++ data/migrations/20210514154906_first-migration.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
       table.increments("project_id");
       table.string("project_name").notNullable();
       table.string("project_description");
-      table.boolean("project_completed").defaultTo("false");
+      table.boolean("project_completed").defaultTo(false);
     })
     .createTable("resources", (table) => {
       table.increments("resource_id");
@@ -15,7 +15,7 @@ exports.up = function (knex) {
       table.increments("task_id");
       table.string("task_description").notNullable();
       table.string("task_notes");
-      table.boolean("task_completed").defaultTo("false");
+      table.boolean("task_completed").defaultTo(false);
       table
         .integer("project_id")
         .unsigned()
@@ -45,6 +45,7 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
   return knex.schema
+    .dropTableIfExists("project_resources")
     .dropTableIfExists("tasks")
     .dropTableIfExists("resources")
     .dropTableIfExists("projects");
