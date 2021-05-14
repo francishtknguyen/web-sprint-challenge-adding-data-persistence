e467408340b8deb4d18492af48b7935d87127896
Francis Nguyen <francishtknguyen@gmail.com>
2021-05-14 11:38:49 -0500

    wsc - db set up server and routers

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
-// build your server here and require it from index.js
+const express = require("express");
+
+const projectRouter = require("../api/project/router");
+const resourceRouter = require("../api/resource/router");
+const taskRouter = require("../api/task/router");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api", projectRouter);
+server.use("/api", resourceRouter);
+server.use("/api", taskRouter);
+
+server.use((err, req, res, next /*eslint-disable-line*/) => {
+  res.status(res.status || 500).json({
+    notes: "Houston, we have a problem....",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
