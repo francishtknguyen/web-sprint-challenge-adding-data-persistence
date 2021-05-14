bcc92aeb7212a6961c4ef1f904be497cd26ca99e
Francis Nguyen <francishtknguyen@gmail.com>
2021-05-14 12:14:41 -0500

    wsc - db completed tasks routes

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,12 @@
-// build your `Task` model here
+const db = require("../../data/dbConfig");
+
+module.exports = { get, create };
+
+function get() {
+  return db("tasks");
+}
+
+async function create(post) {
+  const [task_id] = await db("tasks").insert(post);
+  return db("tasks").where({ task_id });
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,4 +1,19 @@
 const router = require("express").Router();
 const Tasks = require("./model");
 
+router.get("/tasks", (req, res, next) => {
+  Tasks.get()
+    .then((task) => {
+      res.status(200).json(task);
+    })
+    .catch(next);
+});
+
+router.post("/tasks", (req, res, next) => {
+  Tasks.create(req.body)
+    .then((newTask) => {
+      res.status(201).json(newTask);
+    })
+    .catch(next);
+});
 module.exports = router;
